70f4c1455e1317557162dcde629a66777e776857
Sveta Selivanova <129683999+SVETASEL@users.noreply.github.com>
2024-02-13 15:08:09 -0300

    Add files via upload

---
+++ assets/index-53afa24b.js
@@ -0,0 +1,1 @@
+import{c as t,b as i,j as o}from"./index-22fa25b4.js";const d=({className:s,color:e="white"})=>{const r=t(i.divider,s);return o.jsx("div",{className:r,style:{backgroundColor:e}})};export{d as D};
